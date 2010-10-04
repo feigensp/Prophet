3f47c578e3475e04e1970da2a19bf44e13255ec8
koeppen <koeppen@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-10-04 14:59:25 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@15 9da66865-35ea-40ca-919a-acafe8e375a9

--- test3.java
+++ test3.java
@@ -1,37 +1,6 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JLabel;
-import com.swtdesigner.FocusTraversalOnArray;
-import java.awt.Component;
-import javax.swing.JTabbedPane;
-import javax.swing.JPanel;
-import javax.swing.JLayeredPane;
-import javax.swing.Icon;
-import javax.swing.JTree;
-import java.awt.BorderLayout;
-import javax.swing.JInternalFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.DefaultMutableTreeNode;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
-import javax.swing.JSeparator;
-import javax.swing.JEditorPane;
-import javax.swing.JDesktopPane;
-import javax.swing.JTextPane;
-import javax.swing.JScrollBar;
-import javax.swing.JTextArea;
 
 
 public class test3 {
