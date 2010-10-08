541254826e3f4a33a9ec9a7b78af74a3c31603db
koeppen <koeppen@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-10-08 04:11:38 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@37 9da66865-35ea-40ca-919a-acafe8e375a9

--- test/TestMain.java
+++ test/TestMain.java
@@ -4,8 +4,6 @@ package test;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.EventQueue;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.io.File;
 
 import javax.swing.JFrame;
