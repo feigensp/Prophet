2baf3128d4614879a3b8952bb58c49b5d4a0b05a
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-10-26 07:22:15 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@94 9da66865-35ea-40ca-919a-acafe8e375a9

--- lexerTest/Main.java
+++ lexerTest/Main.java
@@ -1,35 +1,41 @@
 package lexerTest;
 
 /*
-Commented By: Christopher Lopes
-File Name: Main.java
-To Create: After the scanner, lcalc.flex, and the parser, ycalc.cup, have been
-           created.
-                > javac Main.java
-To Run: > java Main test.txt
-                where test.txt is an test input file for the calculator.
-*/
+ Commented By: Christopher Lopes
+ File Name: Main.java
+ To Create: After the scanner, lcalc.flex, and the parser, ycalc.cup, have been
+ created.
+ > javac Main.java
+ To Run: > java Main test.txt
+ where test.txt is an test input file for the calculator.
+ */
 
 /* Import classes needed.  The class we created for the parser, the standard
-   runtime class for java, and an io class.*/
+ runtime class for java, and an io class.*/
 import java.io.FileReader;
+import java.io.IOException;
 
 class Main {
 
-  static boolean do_debug_parse = false;
-  static public void main(String argv[]) {
+	static boolean do_debug_parse = false;
 
-  /* Start the parser */
-  try {
-      parser p = new parser(new Lexer(new FileReader("test.txt")));
-      Object result = p.parse().value;
- 
+	static public void main(String argv[]) {
 
-    } catch (Exception e) {
-    	System.out.println("Fehler");
-      /* do cleanup here -- possibly rethrow e */
-      } finally {
-        /* do close out here */
-        }
-  }
+		/* Start the parser */
+		FileReader fr = null;
+		try {
+			fr = new FileReader("src\\lexerTest\\test.txt");
+
+		} catch (Exception e) {
+			System.out.println("Fehler");
+		} finally {
+		}
+		parser p = new parser(new Lexer(fr));
+		try {
+			Object result = p.parse().value;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
