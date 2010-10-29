e066f71a06faaf3436d12f8d7177970b6f43d05f
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-10-29 10:06:33 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@108 9da66865-35ea-40ca-919a-acafe8e375a9

--- questionEditor/Settings.java
+++ questionEditor/Settings.java
@@ -1,10 +1,15 @@
 package questionEditor;
 
+/**
+ * A small class, it only stores an final arraylist with strings.
+ * Every String represents a feature which could be selected oder deselected (in settingsDialog)
+ */
+
 import java.util.ArrayList;
 
 public class Settings {
 	
-	public static ArrayList<String> settings = new ArrayList<String>() {
+	public static final ArrayList<String> settings = new ArrayList<String>() {
 		{
 			add("Editierbar");
 			add("Suchfunktion");
