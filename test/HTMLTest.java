d4d99568bfcdbf05ecc6185ceb597620c336ba7c
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-11-06 19:37:34 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@135 9da66865-35ea-40ca-919a-acafe8e375a9

--- test/HTMLTest.java
+++ test/HTMLTest.java
@@ -5,12 +5,14 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Element;
 import javax.swing.text.html.HTMLDocument;
@@ -55,7 +57,7 @@ public class HTMLTest extends JFrame {
 		textPane.setEditorKit(new HTMLEditorKit());
 		textPane.setEditable(false);
 		textPane.setText("<form>" + 
-				"frage 1 test<br><input name=\"vorname\" type=\"text\" size=\"30\">" +
+				"frage 1 test<br><input id=\"vorname\" type=\"text\" size=\"5\">" +
 				"<br><br><input name ='nextQuestion' type='submit' value='Weiter'>" + 
 				"<form>");
 		contentPane.add(textPane, BorderLayout.CENTER);
@@ -64,8 +66,14 @@ public class HTMLTest extends JFrame {
 			public void actionPerformed(ActionEvent ae) {
 				HTMLDocument doc = (HTMLDocument) textPane.getDocument();
 				Element ele = doc.getElement("vorname");
+				AttributeSet attributes = ele.getAttributes();
+				Enumeration attname = attributes.getAttributeNames();
+				while(attname.hasMoreElements()) {
+					Object e = attname.nextElement();
+					System.out.println(e + ":" + attributes.getAttribute(e));
+				}
 				try {
-					doc.setOuterHTML(ele, "<input id=\"vorname\" type=\"text\" size=\"30\" value=\"hey\">");
+					doc.setOuterHTML(ele, "<input id=\"vorname\" type=\"text\" size=\"60\" value=\"hey\">");
 				} catch (BadLocationException e) {
 					e.printStackTrace();
 				} catch (IOException e) {
