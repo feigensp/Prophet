7526ece0f67a12f23c692bc46e6e7389b92d9bb8
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-12-09 09:13:38 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@269 9da66865-35ea-40ca-919a-acafe8e375a9

---
+++ test/StartExternProg.java
@@ -0,0 +1,78 @@
+package test;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class StartExternProg extends JFrame {
+
+	private JPanel contentPane;
+	private Process p;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					StartExternProg frame = new StartExternProg();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public StartExternProg() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+
+		JButton button = new JButton("New button");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					p = Runtime.getRuntime().exec("C:\\Program Files\\Internet Explorer\\iexplore.exe");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		contentPane.add(button, BorderLayout.WEST);
+
+		JButton button_1 = new JButton("New button");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				OutputStreamWriter outWriter = new OutputStreamWriter(p.getOutputStream());
+				try {
+//					outWriter.write("ETX");
+					outWriter.flush();
+					outWriter.close();
+					p.destroy();
+
+				} catch (Exception io) {
+					io.printStackTrace();
+				}
+			}
+		});
+		contentPane.add(button_1, BorderLayout.EAST);
+	}
+
+}
