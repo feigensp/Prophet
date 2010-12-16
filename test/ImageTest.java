207e316583d92db0da61fa1790a368e25a817dfc
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-12-16 06:19:53 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@282 9da66865-35ea-40ca-919a-acafe8e375a9

--- test/ImageTest.java
+++ test/ImageTest.java
@@ -2,10 +2,13 @@ package test;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.net.URL;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.html.HTMLEditorKit;
 
 public class ImageTest extends JFrame {
 
@@ -35,8 +38,17 @@ public class ImageTest extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setEditorKit(new HTMLEditorKit());
+		textPane.setEnabled(false);
+		URL trueBase = ClassLoader.getSystemResource(".");
+		System.out.println(trueBase);
+		((javax.swing.text.html.HTMLDocument)textPane.getDocument()).setBase(trueBase);
+		textPane.setText("<img src=\"test\\tanzmaus.png\">");
+		contentPane.add(textPane, BorderLayout.CENTER);
 	}
 
 }
