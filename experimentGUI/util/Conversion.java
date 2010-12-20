273d317fe47d1dc3872cc26fc40b21608af4d0e5
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2010-12-20 10:19:49 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@307 9da66865-35ea-40ca-919a-acafe8e375a9

--- experimentGUI/util/Conversion.java
+++ experimentGUI/util/Conversion.java
@@ -4,19 +4,22 @@ package experimentGUI.util;
  * File which contains useful conversion methods
  * 
  * @author Markus Köppen, Andreas Hasselberg
- *
+ * 
  */
 public class Conversion {
 
 	/**
-	 * converts a (Linux) file path to a Windows file path
-	 * @param path path which should be converted
+	 * converts a (Linux) file path to a Windows file path, if the program runs
+	 * on a Windows System
+	 * 
+	 * @param path
+	 *            path which should be converted
 	 * @return windows path
 	 */
 	public static String toWindowsPath(String path) {
-		if(path != null && System.getProperty("file.separator").equals("\\")) {
+		if (path != null && System.getProperty("file.separator").equals("\\")) {
 			return path.replaceAll("/", "\\");
-		} 
+		}
 		return path;
 	}
 }
