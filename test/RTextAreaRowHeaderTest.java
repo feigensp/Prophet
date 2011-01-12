61b605355d388e4ce6c00d9dcb69570a40ee7036
hasselbe <hasselbe@9da66865-35ea-40ca-919a-acafe8e375a9>
2011-01-12 13:25:40 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@370 9da66865-35ea-40ca-919a-acafe8e375a9

--- test/RTextAreaRowHeaderTest.java
+++ test/RTextAreaRowHeaderTest.java
@@ -1,19 +1,19 @@
 package test;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.BadLocationException;
 
 import org.fife.ui.rtextarea.RTextArea;
 import org.fife.ui.rtextarea.RTextScrollPane;
@@ -22,6 +22,7 @@ public class RTextAreaRowHeaderTest extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel drawPanel;
+	private RTextArea textArea;
 	int lastX=0;
 	int lastY=0;
 
@@ -51,7 +52,7 @@ public class RTextAreaRowHeaderTest extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		RTextArea textArea = new RTextArea();
+		textArea = new RTextArea();
 		RTextScrollPane scrollPane = new RTextScrollPane(textArea);
 
 		Component lineNumbers = scrollPane.getRowHeader().getComponent(0);
@@ -60,22 +61,23 @@ public class RTextAreaRowHeaderTest extends JFrame {
 		rowHeader.setLayout(new BorderLayout());
 		rowHeader.add(lineNumbers, BorderLayout.WEST);
 		rowHeader.add(drawPanel, BorderLayout.CENTER);
-		
 		lastX = textArea.getFontMetrics(textArea.getFont()).getHeight();
-		System.out.println(scrollPane.getRowHeader().getAlignmentY());
-		System.out.println(scrollPane.getRowHeader().getBaseline(0, 0));
-		System.out.println(scrollPane.getRowHeader().getY());
-		System.out.println(scrollPane.getRowHeader().getViewPosition());
-		
+		rowHeader.setPreferredSize(new Dimension(50, rowHeader.getHeight()));
 		scrollPane.setRowHeaderView(rowHeader);
 
 		JButton button = new JButton("press me");
 
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent ae) {
-				Graphics g = drawPanel.getGraphics();
-				g.drawLine(0, lastX*51, 10, lastX*51);
-				g.drawLine(0, lastX*50, 10, lastX*50);
+				try {
+					drawRect(textArea, 0, 1, Color.RED);
+				} catch (BadLocationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+//				Graphics g = drawPanel.getGraphics();
+//				g.drawLine(0, lastX*51, 10, lastX*51);
+//				g.drawLine(0, lastX*50, 10, lastX*50);
 			}
 		});
 		contentPane.setLayout(new BorderLayout());
@@ -83,9 +85,14 @@ public class RTextAreaRowHeaderTest extends JFrame {
 		contentPane.add(button, BorderLayout.SOUTH);
 	}
 
-	protected void paintComponent(Graphics g) {
-		Graphics2D g2 = (Graphics2D) drawPanel.getGraphics();
-		g2.drawRect(5, 5, 10, 10);
+	private void drawRect(RTextArea textArea, int offset, int length, Color color) throws BadLocationException {
+		final int rectWidth = 5;
+		int startLineNumber = textArea.getLineOfOffset(offset);
+		int endLineNumber = textArea.getLineOfOffset(offset + length);
+		int rowHeight = textArea.getFontMetrics(textArea.getFont()).getHeight();
+		Graphics g = drawPanel.getGraphics();
+		g.setColor(color);
+		g.fillRect(10, rowHeight*startLineNumber, rectWidth, rowHeight*(endLineNumber-startLineNumber+1));
 	}
 
 }
