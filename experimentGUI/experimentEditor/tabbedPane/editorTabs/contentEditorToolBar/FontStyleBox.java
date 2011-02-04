2d2117bac5dac988a09f0d4bfe6e802306e04aa0
koeppen <koeppen@9da66865-35ea-40ca-919a-acafe8e375a9>
2011-02-04 19:28:26 +0000

    git-svn-id: https://faracvs.cs.uni-magdeburg.de/svn/koeppen-QuelltextProj/src@436 9da66865-35ea-40ca-919a-acafe8e375a9

---
+++ experimentGUI/experimentEditor/tabbedPane/editorTabs/contentEditorToolBar/FontStyleBox.java
@@ -0,0 +1,45 @@
+package experimentGUI.experimentEditor.tabbedPane.editorTabs.contentEditorToolBar;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JComboBox;
+
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+
+import experimentGUI.util.Pair;
+
+
+@SuppressWarnings("serial")
+public class FontStyleBox extends JComboBox implements ActionListener{
+	
+	private RSyntaxTextArea editArea;
+	private ArrayList<Pair<String, String>> fontStyles;
+	
+	public FontStyleBox(RSyntaxTextArea editArea) {
+		super();
+		this.editArea = editArea;
+		fontStyles = new ArrayList<Pair<String, String>>();
+		fontStyles.add(new Pair<String, String>("Fett", "b"));
+		fontStyles.add(new Pair<String, String>("Kursiv", "i"));
+		fontStyles.add(new Pair<String, String>("Unterstrichen", "u"));
+		
+		this.addItem("Schrifttyp");
+		for(int i=0; i<fontStyles.size(); i++) {
+			this.addItem(fontStyles.get(i).getKey());
+		}
+		this.addActionListener(this);
+	}
+	
+	public void actionPerformed(ActionEvent ae) {
+		if (this.getSelectedIndex() != 0) {
+			String tag = fontStyles.get(this.getSelectedIndex()-1).getValue();
+			String text = editArea.getSelectedText();
+			text = text==null ? "" : text;
+			editArea.replaceSelection("<"+tag+">"+text+"</"+tag+">");
+			this.setSelectedIndex(0);
+		}		
+	}
+
+}
