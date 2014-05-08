ef710d44736755698b6197d05d4e00798ce0d118
Georg Seibt <seibt@fim.uni-passau.de>
2014-05-08 15:59:11 +0200

    plugin umkremplung begonnen

---
+++ src/de/uni_passau/fim/infosun/prophet/experimentGUI/util/settings/Setting.java
@@ -0,0 +1,43 @@
+package de.uni_passau.fim.infosun.prophet.experimentGUI.util.settings;
+
+import javax.swing.JPanel;
+
+import de.uni_passau.fim.infosun.prophet.experimentGUI.util.qTree.Attribute;
+
+/**
+ * Implementations of this class represent a value setting (e.g. for a <code>Plugin</code>) that is stored in an
+ * <code>Attribute</code> attached to a <code>QTreeNode</code>.
+ *
+ * Depending on the type of attribute implementing classes should choose an appropriate graphical representation and
+ * controls that enable editing the value.
+ */
+public abstract class Setting extends JPanel {
+
+    protected Attribute attribute;
+
+    /**
+     * Constructs a new <code>Setting</code> for the specified <code>Attribute</code>.
+     *
+     * @param attribute the <code>Attribute</code> for this <code>Setting</code>
+     */
+    public Setting(Attribute attribute) {
+        this.attribute = attribute;
+    }
+
+    /**
+     * Sets the description for this <code>Settings</code> object.
+     *
+     * @param caption the new caption
+     */
+    public abstract void setCaption(String caption);
+
+    /**
+     * Loads the current value of the <code>Attribute</code> and displays it so it can be edited.
+     */
+    public abstract void loadValue();
+
+    /**
+     * Saves the current value in the <code>Attribute</code>.
+     */
+    public abstract void saveValue();
+}
