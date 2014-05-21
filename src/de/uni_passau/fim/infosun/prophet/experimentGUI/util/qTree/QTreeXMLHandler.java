b5139b01905882d81a20e5567ee4f017a16240ad
Georg Seibt <seibt@fim.uni-passau.de>
2014-05-21 16:07:06 +0200

    answerStream configuration, fallback methods to load old-style XML

--- src/de/uni_passau/fim/infosun/prophet/experimentGUI/util/qTree/QTreeXMLHandler.java
+++ src/de/uni_passau/fim/infosun/prophet/experimentGUI/util/qTree/QTreeXMLHandler.java
@@ -3,28 +3,34 @@ package de.uni_passau.fim.infosun.prophet.experimentGUI.util.qTree;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.XStreamException;
+import org.cdmckay.coffeedom.Document;
+import org.cdmckay.coffeedom.Element;
+import org.cdmckay.coffeedom.input.SAXBuilder;
 
 /**
  * Handles XML operations for the <code>QTree</code>.
  */
 public class QTreeXMLHandler {
 
-    private static XStream saveLoadStream;
-    private static XStream answerStream;
+    private static final XStream saveLoadStream;
+    private static final XStream answerStream;
 
     static {
         saveLoadStream = new XStream();
 
         // QTreeNode XML adjustments
-        saveLoadStream.alias("QTreeNode", QTreeNode.class);
+        String simpleName = QTreeNode.class.getSimpleName();
+        saveLoadStream.alias(simpleName, QTreeNode.class);
         saveLoadStream.useAttributeFor(QTreeNode.class, "type");
         saveLoadStream.useAttributeFor(QTreeNode.class, "name");
-        saveLoadStream.addImplicitCollection(QTreeNode.class, "children", "child", QTreeNode.class);
         saveLoadStream.addImplicitMap(QTreeNode.class, "attributes", "attribute", Attribute.class, "key");
+        saveLoadStream.addImplicitCollection(QTreeNode.class, "children", simpleName, QTreeNode.class);
         saveLoadStream.omitField(QTreeNode.class, "answers");
         saveLoadStream.omitField(QTreeNode.class, "answerTime");
 
@@ -35,7 +41,15 @@ public class QTreeXMLHandler {
         saveLoadStream.addImplicitMap(Attribute.class, "subAttributes", "attribute", Attribute.class, "key");
 
         answerStream = new XStream();
-        //TODO configure the answerStream to produce the old answers.xml format
+        answerStream.alias(simpleName, QTreeNode.class);
+        answerStream.useAttributeFor(QTreeNode.class, "type");
+        answerStream.useAttributeFor(QTreeNode.class, "name");
+        answerStream.omitField(QTreeNode.class, "html");
+        answerStream.omitField(QTreeNode.class, "attributes");
+        answerStream.omitField(QTreeNode.class, "parent");
+        answerStream.addImplicitCollection(QTreeNode.class, "children", simpleName, QTreeNode.class);
+        answerStream.addImplicitMap(QTreeNode.class, "answers", "answer", String.class, "name");
+        answerStream.useAttributeFor(QTreeNode.class, "answerTime");
     }
 
     /**
@@ -69,7 +83,8 @@ public class QTreeXMLHandler {
         try {
             node = (QTreeNode) saveLoadStream.fromXML(xmlFile);
         } catch (XStreamException | ClassCastException e) {
-            System.err.println("Could not de-serialise " + xmlFile.getName() + " to a QTreeNode. " + e); //TODO logging?
+            System.err.println(e.getClass().getSimpleName() + " while de-serialising. Falling back to old-style XML.");
+            node = loadOldExperimentXML(xmlFile);
         }
 
         return node;
@@ -89,5 +104,136 @@ public class QTreeXMLHandler {
         Objects.requireNonNull(saveFile, "saveFile must not be null!");
 
         saveLoadStream.toXML(root, new FileWriter(saveFile));
+    }
+
+    // Code to handle the old-style XML format below.
+
+    public final static String TYPE_EXPERIMENT = "experiment";
+    public final static String TYPE_CATEGORY = "category";
+    public final static String TYPE_QUESTION = "question";
+    public final static String TYPE_ATTRIBUTE = "attribute";
+    public final static String TYPE_ATTRIBUTES = "attributes";
+    public final static String TYPE_CHILDREN = "children";
+    public final static String ATTRIBUTE_NAME = "name";
+    public final static String ATTRIBUTE_VALUE = "value";
+
+    /**
+     * Loads the <code>QTreeNode</code> from an old-style XML file.
+     * If there is an error de-serialising the <code>xmlFile</code> <code>null</code> will be returned.
+     *
+     * @param xmlFile the old-style XML file containing a question tree
+     * @return the <code>QTreeNode</code> resulting from converting the file or <code>null</code>
+     */
+    public static QTreeNode loadOldExperimentXML(File xmlFile) {
+        Objects.requireNonNull(xmlFile, "xmlFile must not be null!");
+
+        SAXBuilder builder = new SAXBuilder();
+        Document document;
+        Element element;
+
+        try {
+            document = builder.build(xmlFile);
+        } catch (IOException e) {
+            System.err.println("Could not de-serialise " + xmlFile.getName() + "using old-style XML to a QTreeNode. "
+                     + e);
+            return null;
+        }
+
+        element = document.getRootElement();
+
+        if (!element.getName().equals(TYPE_EXPERIMENT)) {
+            System.err.println("Tried to de-serialise an XML file that did not conform to the old-style XML format.");
+            return null;
+        }
+
+        return loadOldTreeNode(element, null);
+    }
+
+    /**
+     * Loads a <code>QTreeNode</code> from the given <code>Element</code>.
+     *
+     * @param element the <code>Element</code> containing information to be de-serialised to a <code>QTreeNode</code>
+     * @param parent the parent of the resulting <code>QTreeNode</code>
+     * @return the resulting <code>QTreeNode</code>
+     */
+    private static QTreeNode loadOldTreeNode(Element element, QTreeNode parent) {
+        QTreeNode node;
+        QTreeNode.Type type;
+        String name;
+        String html;
+        Element attributes;
+        Element children;
+        List<Element> attributeNodes;
+        List<Element> childNodes;
+
+        switch (element.getName()) {
+            case TYPE_EXPERIMENT:
+                type = QTreeNode.Type.EXPERIMENT;
+                break;
+            case TYPE_CATEGORY:
+                type = QTreeNode.Type.CATEGORY;
+                break;
+            case TYPE_QUESTION:
+                type = QTreeNode.Type.QUESTION;
+                break;
+            default:
+                System.err.println("Unknown node type: " + element.getName());
+                type = null;
+        }
+        name = element.getAttributeValue(ATTRIBUTE_NAME);
+        html = element.getAttributeValue(ATTRIBUTE_VALUE);
+        node = new QTreeNode(parent, type, name);
+
+        node.setHtml(html);
+
+        attributes = element.getChild(TYPE_ATTRIBUTES);
+        children = element.getChild(TYPE_CHILDREN);
+
+        if (attributes != null) {
+            attributeNodes = attributes.getChildren(TYPE_ATTRIBUTE);
+            attributeNodes.forEach(attElement -> node.addAttribute(loadOldAttributeNode(attElement)));
+        }
+
+        if (children != null) {
+
+            childNodes = new LinkedList<>();
+            switch (node.getType()) {
+
+                case EXPERIMENT:
+                    childNodes.addAll(children.getChildren(TYPE_CATEGORY));
+                    break;
+                case CATEGORY:
+                    childNodes.addAll(children.getChildren(TYPE_QUESTION));
+                    break;
+            }
+
+            childNodes.forEach(childElement -> node.addChild(loadOldTreeNode(childElement, node)));
+        }
+
+        return node;
+    }
+
+    /**
+     * Loads an <code>Attribute</code> from the given <code>Element</code>.
+     *
+     * @param element the <code>Element</code> containing information to be de-serialised to an <code>Attribute</code>
+     * @return the resulting <code>Attribute</code>
+     */
+    private static Attribute loadOldAttributeNode(Element element) {
+        Attribute attribute;
+        Element attributes;
+        List<Element> attributeNodes;
+        String key = element.getAttributeValue(ATTRIBUTE_NAME);
+        String value = element.getAttributeValue(ATTRIBUTE_VALUE);
+
+        attribute = new Attribute(key, value);
+        attributes = element.getChild(TYPE_ATTRIBUTES);
+
+        if (attributes != null) {
+            attributeNodes = element.getChildren(TYPE_ATTRIBUTE);
+            attributeNodes.forEach(attElement -> attribute.addSubAttribute(loadOldAttributeNode(attElement)));
+        }
+
+        return attribute;
     }
 }
