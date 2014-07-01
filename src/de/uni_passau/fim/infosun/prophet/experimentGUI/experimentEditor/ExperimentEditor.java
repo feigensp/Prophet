ea77e08deb40428b8ac7992a9f12da178f6b2aa7
Georg Seibt <seibt@fim.uni-passau.de>
2014-07-01 23:32:21 +0200

    language from properties file named 'config'

--- src/de/uni_passau/fim/infosun/prophet/experimentGUI/experimentEditor/ExperimentEditor.java
+++ src/de/uni_passau/fim/infosun/prophet/experimentGUI/experimentEditor/ExperimentEditor.java
@@ -3,7 +3,16 @@ package de.uni_passau.fim.infosun.prophet.experimentGUI.experimentEditor;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JSplitPane;
@@ -24,6 +33,9 @@ import de.uni_passau.fim.infosun.prophet.experimentGUI.util.qTree.QTree;
  * @author Markus Köppen
  */
 public class ExperimentEditor extends JFrame {
+
+    private static final String CONFIG_FILENAME = "config";
+    private Properties config;
 
     /**
      * Shows the GUI of the <code>ExperimentEditor</code>.
@@ -49,17 +61,9 @@ public class ExperimentEditor extends JFrame {
      * sets some basic settings and adds used components
      */
     public ExperimentEditor() {
-        String selectedLanguage = getPreferredLanguage();
 
-        // TODO initialize with selected language
-        Locale locale = Locale.ENGLISH;
-        if (selectedLanguage.equals("English")) {
-            locale = Locale.ENGLISH;
-        } else if (selectedLanguage.equals("German")) {
-            locale = Locale.GERMAN;
-        }
-
-        UIElementNames.setUIElements(locale);
+        loadConfig();
+        initLanguage();
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle(getClass().getSimpleName());
@@ -87,17 +91,78 @@ public class ExperimentEditor extends JFrame {
         setJMenuBar(menuBar);
     }
 
-    private String getPreferredLanguage() {
-        //TODO better modularization
-        Object[] possibleValues = {"German", "English"};
-        Object selectedLanguage = JOptionPane
-                .showInputDialog(null, "Select a language for PROPHET", "Input", JOptionPane.INFORMATION_MESSAGE, null,
-                        possibleValues, possibleValues[0]);
-        if (selectedLanguage == null) {
-            selectedLanguage = "English";
-        }
-        System.out.println("Language: " + selectedLanguage);
+    /**
+     * Loads the 'config' file as a <code>Properties</code> object. If there is no such file an empty
+     * <code>Properties</code> object will be used. Also attaches a window listener that writes the config file to
+     * disk when the current <code>Window</code> is closing.
+     */
+    private void loadConfig() {
+        File configFile = new File(CONFIG_FILENAME);
 
-        return selectedLanguage.toString();
+        config = new Properties();
+        if (configFile.exists() && !configFile.isDirectory()) {
+            try {
+                config.load(new FileInputStream(configFile));
+            } catch (IOException | IllegalArgumentException e) {
+                System.err.println("Could not load properties file " + configFile.getAbsolutePath());
+            }
+        }
+
+        addWindowListener(new WindowAdapter() {
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+
+                try {
+                    config.store(new FileOutputStream(configFile), null);
+                } catch (IOException e1) {
+                    System.err.println("Could not store properties file " + configFile.getAbsolutePath());
+                }
+
+                super.windowClosing(e);
+            }
+        });
+    }
+
+    /**
+     * Initialises the <code>Locale</code> this <code>ExperimentEditor</code> is using. If no language is defined
+     * in the config file the user is asked to select one.
+     */
+    private void initLanguage() {
+        String langKey = "language";
+        String language = config.getProperty(langKey);
+        Locale locale;
+
+        if (language == null) {
+            locale = getPreferredLanguage();
+            config.setProperty(langKey, locale.toLanguageTag());
+        } else {
+            locale = Locale.forLanguageTag(language);
+        }
+
+        UIElementNames.setUIElements(locale);
+    }
+
+    /**
+     * Shows a dialog asking the user to select a language. If the user makes no selection <code>Locale.ENGLISH</code>
+     * is returned.
+     *
+     * @return the selected <code>Locale</code>
+     */
+    private Locale getPreferredLanguage() {
+        Map<String, Locale> languages = new HashMap<>();
+        String message = "Select a language for PROPHET";
+        String title = "Input";
+        Object selectedLanguage;
+        Object[] values;
+
+        languages.put("German", Locale.forLanguageTag("de"));
+        languages.put("English", Locale.forLanguageTag("en"));
+
+        values = languages.keySet().toArray();
+        selectedLanguage = JOptionPane.showInputDialog(null, message, title, JOptionPane.INFORMATION_MESSAGE, null,
+                values, values[0]);
+
+        return languages.getOrDefault(selectedLanguage, Locale.ENGLISH);
     }
 }
