33d28137f5c94cc3211da8ef2bd4f9ce86347601
Georg Seibt <seibt@fim.uni-passau.de>
2014-07-02 14:56:30 +0200

    refactoring, documentation

--- src/de/uni_passau/fim/infosun/prophet/experimentGUI/experimentEditor/tabbedPane/editorTabs/NoteEditorPanel.java
+++ src/de/uni_passau/fim/infosun/prophet/experimentGUI/experimentEditor/tabbedPane/editorTabs/NoteEditorPanel.java
@@ -14,59 +14,64 @@ import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
 /**
- * A panel for taking notes for every node in the tree.
+ * Displays a simple editable document allowing the user to save notes for every <code>QTreeNode</code>.
  *
  * @author Andreas Hasselberg
  * @author Markus Köppen
  */
-@SuppressWarnings("serial")
 public class NoteEditorPanel extends ExperimentEditorTab {
 
-    private Map<QTreeNode, RTextScrollPane> scrollPanes;
-    private Map<QTreeNode, RSyntaxTextArea> editAreas = new HashMap<>();
-    private QTreeNode selected;
     public final static String KEY_NOTES = "notes";
 
     /**
-     * Constructor
+     * Caches for the various layout components to preserve their state when the user switches between nodes.
+     */
+    private Map<QTreeNode, RTextScrollPane> scrollPanes = new HashMap<>();
+    private Map<QTreeNode, RSyntaxTextArea> editAreas = new HashMap<>();
+
+    private QTreeNode selected;
+
+    /**
+     * Constructs a new empty <code>NoteEditorPanel</code>.
      */
     public NoteEditorPanel() {
         setLayout(new BorderLayout());
-        this.setOpaque(false);
-        scrollPanes = new HashMap<>();
+        setOpaque(false);
     }
 
-    /**
-     * loads the notes for a selected node into the tab, called by EditorTabbedPane
-     */
     @Override
     public void load(final QTreeNode selected) {
         this.selected = selected;
-        this.removeAll();
-        this.updateUI();
-        if (selected != null) {
-            RTextScrollPane scrollPane = scrollPanes.get(this.selected);
-            if (scrollPane == null) {
-                RSyntaxDocument doc = new RSyntaxDocument(SyntaxConstants.SYNTAX_STYLE_NONE);
-                try {
-                    doc.insertString(0, selected.getAttribute(KEY_NOTES).getValue(), null);
-                } catch (BadLocationException e) {
-                    e.printStackTrace();
-                }
-                RSyntaxTextArea editArea = new ModifiedRSyntaxTextArea(doc);
-                editAreas.put(selected, editArea);
-                editArea.setLineWrap(true);
 
-                scrollPane = new RTextScrollPane(editArea);
-                scrollPanes.put(selected, scrollPane);
-            }
-            add(scrollPane, BorderLayout.CENTER);
+        removeAll();
+        if (selected == null) {
+            return;
         }
+
+        RTextScrollPane scrollPane = scrollPanes.get(this.selected);
+        if (scrollPane == null) {
+            RSyntaxTextArea editArea;
+            RSyntaxDocument doc;
+
+            doc = new RSyntaxDocument(SyntaxConstants.SYNTAX_STYLE_NONE);
+            try {
+                doc.insertString(0, selected.getAttribute(KEY_NOTES).getValue(), null);
+            } catch (BadLocationException e) {
+                System.err.println("Could not display the notes for " + selected + ".");
+            }
+
+            editArea = new ModifiedRSyntaxTextArea(doc);
+            editArea.setLineWrap(true);
+            editAreas.put(selected, editArea);
+
+            scrollPane = new RTextScrollPane(editArea);
+            scrollPanes.put(selected, scrollPane);
+        }
+
+        add(scrollPane, BorderLayout.CENTER);
+        repaint();
     }
 
-    /**
-     * saves any changes, called by EditorTabbedPane
-     */
     @Override
     public void save() {
         if (selected != null) {
