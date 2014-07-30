817d366c79b1a7833a0bd5ec3d767c8234c6d47d
Stefan <brandste@fim.uni-passau.de>
2014-07-30 17:11:30 +0200

    deleted old QuestionTree classes because QTree now has the same functionality

--- src/de/uni_passau/fim/infosun/prophet/experimentGUI/util/questionTree/QuestionTreeNodeListener.java
+++
@@ -1,6 +0,0 @@
-package de.uni_passau.fim.infosun.prophet.experimentGUI.util.questionTree;
-
-public interface QuestionTreeNodeListener {
-
-    public void questionTreeEventOccured(QuestionTreeNodeEvent e);
-}
