2c169f644f75bcecf3307c49e567e32749478090
Georg Seibt <seibt@fim.uni-passau.de>
2014-08-14 23:11:50 +0200

    documentation

--- src/de/uni_passau/fim/infosun/prophet/experimentGUI/plugin/plugins/ValidSubjectCodePlugin.java
+++ src/de/uni_passau/fim/infosun/prophet/experimentGUI/plugin/plugins/ValidSubjectCodePlugin.java
@@ -19,6 +19,10 @@ import de.uni_passau.fim.infosun.prophet.experimentGUI.util.settings.components.
 
 import static de.uni_passau.fim.infosun.prophet.experimentGUI.util.qTree.QTreeNode.Type.EXPERIMENT;
 
+/**
+ * Plugin that is used to check the subject code the user input against a list of valid codes.
+ * Will only allow advancing past the first node of the experiment of the code matches one of the valid codes.
+ */
 public class ValidSubjectCodePlugin implements Plugin {
 
     private static final String KEY = "valid_code";
