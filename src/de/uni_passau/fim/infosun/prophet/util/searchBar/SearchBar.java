a9f80bf22d01c7521247b0ed97d0afc4ac541916
Georg Seibt <seibt@fim.uni-passau.de>
2014-11-26 13:53:53 +0100

    first readability refactorings

--- src/de/uni_passau/fim/infosun/prophet/util/searchBar/SearchBar.java
+++ src/de/uni_passau/fim/infosun/prophet/util/searchBar/SearchBar.java
@@ -4,12 +4,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.Vector;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JOptionPane;
-import javax.swing.JTextField;
-import javax.swing.JToolBar;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
 
 import de.uni_passau.fim.infosun.prophet.util.language.UIElementNames;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
@@ -17,85 +14,103 @@ import org.fife.ui.rtextarea.SearchContext;
 import org.fife.ui.rtextarea.SearchEngine;
 
 /**
- * This class adds a JTextPane to a searchbar which is created. With this
- * searchBar the user can search through the text in the JTextPane User
+ * A <code>JToolBar</code> containing a controls to enable searching for strings or regular expressions in a
+ * given <code>RSyntaxTextArea</code>.
  *
- * @author Robert Futrell, Markus Köppen, Andreas Hasselberg
+ * @author Robert Futrell
+ * @author Markus Köppen
+ * @author Andreas Hasselberg
+ * @author Georg Seibt
  */
-
 public class SearchBar extends JToolBar implements ActionListener {
 
-    private static final long serialVersionUID = 1L;
-
-    public static final String CAPTION_HIDE = "X";
-    public static final String CAPTION_NEXT = UIElementNames.getLocalized("SEARCH_BAR_SEARCH_FORWARD");
-    public static final String CAPTION_PREVIOUS = UIElementNames.getLocalized("SEARCH_BAR_SEARCH_BACKWARD");
-    public static final String CAPTION_REGEX = UIElementNames.getLocalized("SEARCH_BAR_REGULAR_EXPRESSION");
-    public static final String CAPTION_MATCH_CASE = UIElementNames.getLocalized("SEARCH_BAR_CASE_SENSITIVE");
+    private static final String CAPTION_HIDE = "X";
+    private static final String CAPTION_NEXT = UIElementNames.getLocalized("SEARCH_BAR_SEARCH_FORWARD");
+    private static final String CAPTION_PREVIOUS = UIElementNames.getLocalized("SEARCH_BAR_SEARCH_BACKWARD");
+    private static final String CAPTION_REGEX = UIElementNames.getLocalized("SEARCH_BAR_REGULAR_EXPRESSION");
+    private static final String CAPTION_MATCH_CASE = UIElementNames.getLocalized("SEARCH_BAR_CASE_SENSITIVE");
+    private static final String MESSAGE_NOT_FOUND = UIElementNames.getLocalized("SEARCH_BAR_MESSAGE_TEXT_NOT_FOUND");
 
     public static final String ACTION_HIDE = "Hide";
     public static final String ACTION_NEXT = "FindNext";
     public static final String ACTION_PREVIOUS = "FindPrevious";
 
-    public static final String MESSAGE_NOT_FOUND = UIElementNames.getLocalized("SEARCH_BAR_MESSAGE_TEXT_NOT_FOUND");
+    private JButton hideButton;
+    private JButton nextButton;
+    private JButton previousButton;
+    private JCheckBox regexCB;
+    private JCheckBox matchCaseCB;
+    private JTextField searchField;
 
     private RSyntaxTextArea textArea;
-
-    private JButton hideButton = new JButton(CAPTION_HIDE);
-    private JTextField searchField = new JTextField(30);
-    private JButton forwardButton = new JButton(CAPTION_NEXT);
-    private JButton backwardButton = new JButton(CAPTION_PREVIOUS);
-    private JCheckBox regexCB = new JCheckBox(CAPTION_REGEX);
-    private JCheckBox matchCaseCB = new JCheckBox(CAPTION_MATCH_CASE);
-
-    private Vector<SearchBarListener> listeners = new Vector<>();
-
-    public void addSearchBarListener(SearchBarListener l) {
-        listeners.add(l);
-    }
-
-    public void removeSearchBarListener(SearchBarListener l) {
-        listeners.remove(l);
-    }
+    private List<SearchBarListener> listeners;
 
     /**
-     * Grabs the focus
+     * Constructs a new <code>SearchBar</code> searching through the contents of the given <code>RSyntaxTextArea</code>.
+     *
+     * @param textArea the <code>RSyntaxTextArea</code> to search through
      */
-    @Override
-    public void grabFocus() {
-        searchField.grabFocus();
-    }
-
     public SearchBar(RSyntaxTextArea textArea) {
+        setFloatable(false);
+
         this.textArea = textArea;
-        this.setFloatable(false);
-        // Create a toolbar with searching options.
+        this.listeners = new ArrayList<>();
+
+        hideButton = new JButton(CAPTION_HIDE);
         hideButton.setActionCommand(ACTION_HIDE);
         hideButton.addActionListener(this);
         add(hideButton);
+
+        nextButton = new JButton(CAPTION_NEXT);
+        nextButton.setActionCommand(ACTION_NEXT);
+        nextButton.addActionListener(this);
+        add(nextButton);
+
+        previousButton = new JButton(CAPTION_PREVIOUS);
+        previousButton.setActionCommand(ACTION_PREVIOUS);
+        previousButton.addActionListener(this);
+        add(previousButton);
+
+        searchField = new JTextField(30);
         searchField.addKeyListener(new KeyAdapter() {
 
             @Override
             public void keyPressed(KeyEvent arg0) {
                 if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
-                    forwardButton.doClick();
+                    nextButton.doClick();
                 }
             }
         });
         add(searchField);
-        forwardButton.setActionCommand(ACTION_NEXT);
-        forwardButton.addActionListener(this);
-        add(forwardButton);
-        backwardButton.setActionCommand(ACTION_PREVIOUS);
-        backwardButton.addActionListener(this);
-        add(backwardButton);
+
+        regexCB = new JCheckBox(CAPTION_REGEX);
         add(regexCB);
+
+        matchCaseCB = new JCheckBox(CAPTION_MATCH_CASE);
         add(matchCaseCB);
     }
 
+    /**
+     * Adds a <code>SearchBarListener</code> to this <code>SearchBar</code>.
+     *
+     * @param listener the <code>SearchBarListener</code> to add
+     */
+    public void addSearchBarListener(SearchBarListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Removes a <code>SearchBarListener</code> from this <code>SearchBar</code>.
+     *
+     * @param listener the <code>SearchBarListener</code> to remove
+     */
+    public void removeSearchBarListener(SearchBarListener listener) {
+        listeners.remove(listener);
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e) {
-        String command = e.getActionCommand();
+    public void actionPerformed(ActionEvent event) {
+        String command = event.getActionCommand();
 
         if (command.equals(ACTION_HIDE)) {
             setVisible(false);
@@ -134,41 +149,22 @@ public class SearchBar extends JToolBar implements ActionListener {
         if (!SearchEngine.find(textArea, searchContext).wasFound()) {
             JOptionPane.showMessageDialog(this, MESSAGE_NOT_FOUND);
 
-            for (SearchBarListener l : listeners) {
-                l.searched(command, text, false);
+            for (SearchBarListener listener : listeners) {
+                listener.searched(command, text, false);
             }
         } else {
-            for (SearchBarListener l : listeners) {
-                l.searched(command, text, true);
+            for (SearchBarListener listener : listeners) {
+                listener.searched(command, text, true);
             }
         }
     }
 
-    public RSyntaxTextArea getTextArea() {
-        return textArea;
-    }
-
-    public JButton getHideButton() {
-        return hideButton;
-    }
-
-    public JTextField getSearchField() {
-        return searchField;
-    }
-
-    public JButton getForwardButton() {
-        return forwardButton;
-    }
-
-    public JButton getBackwardButton() {
-        return backwardButton;
+    @Override
+    public void grabFocus() {
+        searchField.grabFocus();
     }
 
     public JCheckBox getRegexCB() {
         return regexCB;
-    }
-
-    public JCheckBox getMatchCaseCB() {
-        return matchCaseCB;
     }
 }
