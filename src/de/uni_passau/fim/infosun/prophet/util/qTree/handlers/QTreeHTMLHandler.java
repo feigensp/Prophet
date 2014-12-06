1b211b489264a6cffa13f19296bdc706f94d8607
Georg Seibt <seibt@fim.uni-passau.de>
2014-12-06 15:27:50 +0100

    explicitly specify UTF-8 as the encoding

--- src/de/uni_passau/fim/infosun/prophet/util/qTree/handlers/QTreeHTMLHandler.java
+++ src/de/uni_passau/fim/infosun/prophet/util/qTree/handlers/QTreeHTMLHandler.java
@@ -1,8 +1,12 @@
 package de.uni_passau.fim.infosun.prophet.util.qTree.handlers;
 
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -183,8 +187,10 @@ public final class QTreeHTMLHandler extends QTreeFormatHandler {
             body.append(divider);
         }
 
-        try (FileWriter out = new FileWriter(saveFile)) {
-            out.write(doc.outerHtml());
+        CharsetEncoder utf8encoder = StandardCharsets.UTF_8.newEncoder();
+
+        try (Writer writer = new OutputStreamWriter(new FileOutputStream(saveFile), utf8encoder)) {
+            writer.write(doc.outerHtml());
         }
     }
 
