d781dc9c8d5f202c1461ae8e5a3bedffd426f412
Georg Seibt <seibt@fim.uni-passau.de>
2015-01-11 15:33:02 +0100

    package documentation

---
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/recorder/loggingTree/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the tree data structure used by the <code>Recorder</code> for logging.
+ */
+package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.recorder.loggingTree;
