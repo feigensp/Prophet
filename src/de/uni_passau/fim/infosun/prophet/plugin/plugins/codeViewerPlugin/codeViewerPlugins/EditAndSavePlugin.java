bbb2a5ea3dc1225f0f735882b484cb4a2abc62fa
Georg Seibt <seibt@fim.uni-passau.de>
2015-02-15 17:02:07 +0100

    early returns refactoring, doc

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/EditAndSavePlugin.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/EditAndSavePlugin.java
@@ -50,21 +50,23 @@ public class EditAndSavePlugin implements Plugin {
     public void onCreate(CodeViewer viewer) {
         Attribute attr = viewer.getAttribute();
         editable = attr.containsSubAttribute(KEY) && Boolean.parseBoolean(attr.getSubAttribute(KEY).getValue());
-        
-        if (editable) {
-            tabbedPane = viewer.getTabbedPane();
-            saveDir = new File(viewer.getSaveDir().getPath() + System.getProperty("file.separator") + "savedFiles");
-            isChanged = new HashMap<>();
-            JMenuItem saveMenuItem = new JMenuItem(getLocalized("EDIT_AND_SAVE_SAVE"));
-            saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.Event.CTRL_MASK));
-            viewer.addMenuItemToFileMenu(saveMenuItem);
-            saveMenuItem.addActionListener(e -> saveActiveFile());
-            JMenuItem saveAllMenuItem = new JMenuItem(getLocalized("EDIT_AND_SAVE_SAVE_ALL"));
-            saveAllMenuItem.setAccelerator(KeyStroke
-                    .getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.Event.CTRL_MASK | java.awt.Event.SHIFT_MASK));
-            viewer.addMenuItemToFileMenu(saveAllMenuItem);
-            saveAllMenuItem.addActionListener(event -> saveAllFiles());
+
+        if (!editable) {
+            return;
         }
+
+        tabbedPane = viewer.getTabbedPane();
+        saveDir = new File(viewer.getSaveDir().getPath() + System.getProperty("file.separator") + "savedFiles");
+        isChanged = new HashMap<>();
+        JMenuItem saveMenuItem = new JMenuItem(getLocalized("EDIT_AND_SAVE_SAVE"));
+        saveMenuItem.setAccelerator(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.Event.CTRL_MASK));
+        viewer.addMenuItemToFileMenu(saveMenuItem);
+        saveMenuItem.addActionListener(e -> saveActiveFile());
+        JMenuItem saveAllMenuItem = new JMenuItem(getLocalized("EDIT_AND_SAVE_SAVE_ALL"));
+        saveAllMenuItem.setAccelerator(KeyStroke
+                .getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.Event.CTRL_MASK | java.awt.Event.SHIFT_MASK));
+        viewer.addMenuItemToFileMenu(saveAllMenuItem);
+        saveAllMenuItem.addActionListener(event -> saveAllFiles());
     }
 
     private void saveActiveFile() {
@@ -100,106 +102,136 @@ public class EditAndSavePlugin implements Plugin {
 
     @Override
     public void onEditorPanelCreate(final EditorPanel editorPanel) {
-        if (editable) {
-            RSyntaxTextArea textArea = editorPanel.getTextArea();
-            File savedFile = new File(saveDir.getPath() + editorPanel.getFile().getName());
-            if (savedFile.exists()) {
-                Document doc = textArea.getDocument();
-                DocumentListener[] listeners = removeDocumentListener((RSyntaxDocument) doc);
-                try {
-                    doc.remove(0, doc.getLength());
-                } catch (BadLocationException e) {
-                    e.printStackTrace();
-                }
-                byte[] buffer = new byte[(int) savedFile.length()];
-                FileInputStream fileStream;
-                try {
-                    fileStream = new FileInputStream(savedFile);
-                    fileStream.read(buffer);
-                    doc.insertString(0, new String(buffer), null);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                textArea.setCaretPosition(0);
-                readdDocumentListeners((RSyntaxDocument) doc, listeners);
+        
+        if (!editable) {
+            return;
+        }
+
+        RSyntaxTextArea textArea = editorPanel.getTextArea();
+        File savedFile = new File(saveDir.getPath() + editorPanel.getFile().getName());
+        
+        if (savedFile.exists()) {
+            Document doc = textArea.getDocument();
+            DocumentListener[] listeners = removeListeners((RSyntaxDocument) doc);
+            try {
+                doc.remove(0, doc.getLength());
+            } catch (BadLocationException e) {
+                e.printStackTrace();
             }
-            textArea.setEditable(true);
-            textArea.getDocument().addDocumentListener(new DocumentListener() {
+            byte[] buffer = new byte[(int) savedFile.length()];
+            FileInputStream fileStream;
+            try {
+                fileStream = new FileInputStream(savedFile);
+                fileStream.read(buffer);
+                doc.insertString(0, new String(buffer), null);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            textArea.setCaretPosition(0);
+            addListeners((RSyntaxDocument) doc, listeners);
+        }
+        
+        textArea.setEditable(true);
+        textArea.getDocument().addDocumentListener(new DocumentListener() {
 
-                private void changeOccured() {
-                    isChanged.put(editorPanel, true);
-                }
+            private void changeOccured() {
+                isChanged.put(editorPanel, true);
+            }
 
-                @Override
-                public void changedUpdate(DocumentEvent arg0) {
+            @Override
+            public void changedUpdate(DocumentEvent arg0) {
 //					changeOccured();
-                }
+            }
 
-                @Override
-                public void insertUpdate(DocumentEvent arg0) {
-                    changeOccured();
-                }
+            @Override
+            public void insertUpdate(DocumentEvent arg0) {
+                changeOccured();
+            }
 
-                @Override
-                public void removeUpdate(DocumentEvent arg0) {
-                    changeOccured();
-                }
-            });
-        }
-    }
-
-    private DocumentListener[] removeDocumentListener(RSyntaxDocument doc) {
-        DocumentListener[] listeners = doc.getDocumentListeners();
-        for (DocumentListener listener : listeners) {
-            doc.removeDocumentListener(listener);
-        }
-        return listeners;
-    }
-
-    private void readdDocumentListeners(RSyntaxDocument doc, DocumentListener[] listeners) {
-        for (DocumentListener listener : listeners) {
-            doc.addDocumentListener(listener);
-        }
+            @Override
+            public void removeUpdate(DocumentEvent arg0) {
+                changeOccured();
+            }
+        });
     }
 
     @Override
     public void onEditorPanelClose(EditorPanel editorPanel) {
-        if (editable) {
-            Boolean changed = isChanged.get(editorPanel);
-            if (changed != null && changed) {
-                int n = JOptionPane.showConfirmDialog(null, getLocalized("EDIT_AND_SAVE_DIALOG_SAVE_CHANGES") + "?",
-                        getLocalized("EDIT_AND_SAVE_SAVE") + "?", JOptionPane.YES_NO_OPTION);
-                if (n == JOptionPane.YES_OPTION) {
-                    saveEditorPanel(editorPanel);
-                }
-            }
-            isChanged.remove(editorPanel);
+
+        if (!editable) {
+            return;
         }
+
+        Boolean changed = isChanged.get(editorPanel);
+        if (changed != null && changed) {
+            int n = JOptionPane.showConfirmDialog(null, getLocalized("EDIT_AND_SAVE_DIALOG_SAVE_CHANGES") + "?",
+                    getLocalized("EDIT_AND_SAVE_SAVE") + "?", JOptionPane.YES_NO_OPTION);
+            if (n == JOptionPane.YES_OPTION) {
+                saveEditorPanel(editorPanel);
+            }
+        }
+        isChanged.remove(editorPanel);
     }
 
     @Override
     public void onClose() {
-        if (editable) {
-            boolean ask = false;
-            for (int i = 0; i < tabbedPane.getTabCount(); i++) {
-                Component myComp = tabbedPane.getComponentAt(i);
-                if (myComp instanceof EditorPanel) {
-                    Boolean changed = isChanged.get(myComp);
-                    System.out.println(changed);
-                    if (changed != null && changed) {
-                        ask = true;
-                        break;
-                    }
+
+        if (!editable) {
+            return;
+        }
+
+        boolean ask = false;
+        for (int i = 0; i < tabbedPane.getTabCount(); i++) {
+            Component myComp = tabbedPane.getComponentAt(i);
+            if (myComp instanceof EditorPanel) {
+                Boolean changed = isChanged.get(myComp);
+                System.out.println(changed);
+                if (changed != null && changed) {
+                    ask = true;
+                    break;
                 }
             }
-            if (ask) {
-                int n = JOptionPane.showConfirmDialog(null, getLocalized("EDIT_AND_SAVE_DIALOG_SAVE_CHANGES") + "?",
-                        getLocalized("EDIT_AND_SAVE_SAVE") + "?", JOptionPane.YES_NO_OPTION);
-                if (n == JOptionPane.YES_OPTION) {
-                    saveAllFiles();
-                }
+        }
+        if (ask) {
+            int n = JOptionPane.showConfirmDialog(null, getLocalized("EDIT_AND_SAVE_DIALOG_SAVE_CHANGES") + "?",
+                    getLocalized("EDIT_AND_SAVE_SAVE") + "?", JOptionPane.YES_NO_OPTION);
+            if (n == JOptionPane.YES_OPTION) {
+                saveAllFiles();
             }
-            isChanged = null;
+        }
+        isChanged = null;
+    }
+
+    /**
+     * Removes all <code>DocumentListener</code>s from the given <code>RSyntaxDocument</code> and returns them.
+     *
+     * @param doc
+     *         the <code>RSyntaxDocument</code> to remove listeners from
+     *
+     * @return the removed listeners
+     */
+    private DocumentListener[] removeListeners(RSyntaxDocument doc) {
+        DocumentListener[] listeners = doc.getDocumentListeners();
+
+        for (DocumentListener listener : listeners) {
+            doc.removeDocumentListener(listener);
+        }
+
+        return listeners;
+    }
+
+    /**
+     * Adds all given <code>DocumentListener</code>s to the <code>RSyntaxDocument</code>.
+     *
+     * @param doc
+     *         the <code>RSyntaxDocument</code> to add the <code>DocumentListener</code>s to
+     * @param listeners
+     *         the <code>DocumentListener</code>s to add
+     */
+    private void addListeners(RSyntaxDocument doc, DocumentListener[] listeners) {
+
+        for (DocumentListener listener : listeners) {
+            doc.addDocumentListener(listener);
         }
     }
 }
