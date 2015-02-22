4b4986a26c957b7b7559bccad8ae6a68ef3fe465
Georg Seibt <seibt@fim.uni-passau.de>
2015-02-22 18:43:58 +0100

    doc

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/recorder/RecorderPluginList.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/recorder/RecorderPluginList.java
@@ -1,6 +1,7 @@
 package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.recorder;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,6 +10,14 @@ import de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.recorde
 import de.uni_passau.fim.infosun.prophet.util.qTree.Attribute;
 import de.uni_passau.fim.infosun.prophet.util.settings.Setting;
 
+/**
+ * The <code>Plugin</code> repository of the <code>Recorder</code>. Allows adding and removing <code>Plugin</code>s as
+ * well as calling the methods of the <code>Plugin</code> interface on all currently active <code>Plugin</code>s.
+ *
+ * @author Georg Seibt
+ * @author Andreas Hasselberg
+ * @author Markus Köppen
+ */
 public class RecorderPluginList {
 
     private static List<Plugin> plugins = new ArrayList<>();
@@ -30,14 +39,33 @@ public class RecorderPluginList {
         return plugins.stream().map(p -> p.getSetting(attribute)).filter(s -> s != null).collect(Collectors.toList());
     }
 
+	/**
+	 * Returns an unmodifiable list view of all currently active <code>Plugin</code>s.
+	 *
+	 * @return the list of plugins
+	 */
     public static List<Plugin> getPlugins() {
-        return plugins;
+        return Collections.unmodifiableList(plugins);
     }
 
+	/**
+	 * Adds a <code>Plugin</code> to the currently active plugins.
+	 *
+	 * @param plugin
+	 * 		the plugin to be added
+	 */
     public static void add(Plugin plugin) {
         plugins.add(plugin);
     }
 
+	/**
+	 * Removes a <code>Plugin</code> from the currently active plugins.
+	 *
+	 * @param plugin
+	 * 		the plugin to be removed
+	 *
+	 * @return true iff the <code>Plugin</code> was active and has been removed
+	 */
     public static boolean remove(Plugin plugin) {
         return plugins.remove(plugin);
     }
