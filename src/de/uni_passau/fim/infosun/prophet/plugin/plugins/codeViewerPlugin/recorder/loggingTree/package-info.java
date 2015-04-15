6ed535d402728372daa3006c70f20bef2b9b1aff
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-15 21:37:40 +0200

    removing old recorder code

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/recorder/loggingTree/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Contains the tree data structure used by the <code>Recorder</code> for logging.
- */
-package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.recorder.loggingTree;
--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/recorder/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains the <code>Recorder</code>, all its ancillary classes and the <code>Plugin</code> interface that
- * plugins for the <code>Recorder</code> are required to implement.
- */
-package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.recorder;
