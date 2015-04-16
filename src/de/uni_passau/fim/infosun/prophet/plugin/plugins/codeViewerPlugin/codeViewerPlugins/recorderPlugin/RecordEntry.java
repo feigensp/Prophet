2de1873d90d00cb08ca4f83c4b2cf5851c7741a6
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-16 19:52:55 +0200

    documentation

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/RecordEntry.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/RecordEntry.java
@@ -2,18 +2,31 @@ package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.codeVi
 
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 
+/**
+ * <code>Recorder</code> implementations produce <code>RecordEntry</code>s which are recorded by a <code>Record</code>.
+ * Implementations of this class should use XStream annotations to make sure that the XML that is produced when the
+ * <code>Record</code> is serialized is readable.
+ */
 public abstract class RecordEntry implements Comparable<RecordEntry> {
 
     protected static final String OPENED = "opened";
     protected static final String CLOSED = "closed";
-    
+
     @XStreamAsAttribute
     private long timestamp;
 
+    /**
+     * Constructs a new <code>RecordEntry</code> and records the current time as its timestamp.
+     */
     public RecordEntry() {
         this.timestamp = System.currentTimeMillis();
     }
 
+    /**
+     * Returns the timestampt of this <code>RecordEntry</code>.
+     *
+     * @return the timestamp
+     */
     public long getTimestamp() {
         return timestamp;
     }
