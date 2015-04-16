9cbc08cc92ead412047390e613073271c6e1e592
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-16 19:05:17 +0200

    package documentation

---
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains all implementations of the <code>RecordEntry</code>.
+ */
+package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.codeViewerPlugins.recorderPlugin.recordEntries;
