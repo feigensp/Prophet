eb01d37814bbcfc71969c94531674d5c32d64dfb
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-18 21:16:17 +0200

    doc

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/FileStatusEntry.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/FileStatusEntry.java
@@ -7,12 +7,24 @@ import de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.tabbedP
 
 import java.io.File;
 
+/**
+ * A <code>RecordEntry</code> that is produced when a tab for a file is opened or closed in the <code>CodeViewer</code>.
+ */
 @XStreamAlias("fileEntry")
 public class FileStatusEntry extends EditorPanelEntry {
 
     private Action action;
     private File file;
-    
+
+    /**
+     * Constructs a new <code>FileStatusEntry</code> recording an open/close event for the file of the given
+     * <code>EditorPanel</code>.
+     *
+     * @param panel
+     *         the <code>EditorPanel</code> whose file was opened/closed
+     * @param action
+     *         whether the file of the <code>panel</code> was opened or closed
+     */
     public FileStatusEntry(EditorPanel panel, Action action) {
         super(panel);
         this.action = action;
