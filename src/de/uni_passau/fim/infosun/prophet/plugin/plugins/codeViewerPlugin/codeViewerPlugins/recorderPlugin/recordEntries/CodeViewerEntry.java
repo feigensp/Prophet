dfc9ff5a470beed69bde6019fa4818d661cea29e
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-18 23:15:55 +0200

    doc

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/CodeViewerEntry.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/CodeViewerEntry.java
@@ -7,8 +7,20 @@ import de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.codeVie
 import java.util.Map;
 import java.util.WeakHashMap;
 
+/**
+ * This class should be extended when recording an event that concerns a <code>CodeViewer</code> but no specific
+ * <code>EditorPanel</code> in it. The <code>CodeViewerEntry</code> will add an attribute 'cvId' to the XML which
+ * identifies the <code>CodeViewer</code> in which the event occurred.
+ */
 public abstract class CodeViewerEntry extends RecordEntry {
 
+    /**
+     * Provides <code>Integer</code> IDs for objects of type <code>T</code>. The references to the objects are weak
+     * and will not prevent garbage collection.
+     *
+     * @param <T>
+     *         the type of the objects for which IDs are to be provided
+     */
     protected static class IDProvider<T> {
 
         private Map<T, Integer> currentIDs = new WeakHashMap<>();
@@ -33,6 +45,12 @@ public abstract class CodeViewerEntry extends RecordEntry {
     @XStreamAsAttribute
     private int cvId;
 
+    /**
+     * Constructs a new <code>CodeViewerEntry</code> storing the ID of the given <code>CodeViewer</code>.
+     *
+     * @param viewer
+     *         the <code>CodeViewer</code> in which the event occurred
+     */
     public CodeViewerEntry(CodeViewer viewer) {
         this.cvId = idProvider.idFor(viewer);
     }
--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/EditorPanelEntry.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/EditorPanelEntry.java
@@ -3,6 +3,11 @@ package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.codeVi
 import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
 import de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.tabbedPane.EditorPanel;
 
+/**
+ * This class should be extended when recording an event that occurred in a specific <code>EditorPanel</code>.
+ * The <code>EditorPanelEntry</code> will add an attribute 'tabId' to the XML which identifies the
+ * <code>EditorPanel</code> in which the event occurred.
+ */
 public abstract class EditorPanelEntry extends CodeViewerEntry {
 
     private static IDProvider<EditorPanel> idProvider = new IDProvider<>();
@@ -10,6 +15,12 @@ public abstract class EditorPanelEntry extends CodeViewerEntry {
     @XStreamAsAttribute
     private int tabId;
 
+    /**
+     * Constructs a new <code>EditorPanelEntry</code> storing the ID of the given <code>EditorPanel</code>.
+     *
+     * @param panel
+     *         the <code>EditorPanel</code> in which the event occurred
+     */
     public EditorPanelEntry(EditorPanel panel) {
         super(panel.getCodeViewer());
         this.tabId = idProvider.idFor(panel);
