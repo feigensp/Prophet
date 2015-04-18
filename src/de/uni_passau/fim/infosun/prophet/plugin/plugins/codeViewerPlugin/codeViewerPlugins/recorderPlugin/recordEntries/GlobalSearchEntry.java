59e0bdf34743041096c4954bc76fc22152f71ca9
Georg Seibt <seibt@fim.uni-passau.de>
2015-04-18 21:07:10 +0200

    doc

--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/GlobalSearchEntry.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/GlobalSearchEntry.java
@@ -3,6 +3,10 @@ package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.codeVi
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.CodeViewer;
 
+/**
+ * A <code>RecordEntry</code> that is produced when the search functionality provided by
+ * <code>GlobalSearchBar</code> is used.
+ */
 @XStreamAlias("globalSearchEntry")
 public class GlobalSearchEntry extends CodeViewerEntry {
 
@@ -10,6 +14,18 @@ public class GlobalSearchEntry extends CodeViewerEntry {
     private String query;
     private boolean success;
 
+    /**
+     * Construcs a new <code>GlobalSearchEntry</code> recording a search event in the given <code>CodeViewer</code>.
+     *
+     * @param viewer
+     *         the <code>CodeViewer</code> whose displayed files where searched through
+     * @param action
+     *         the search action command
+     * @param query
+     *         the search query
+     * @param success
+     *         whether the search was successful
+     */
     public GlobalSearchEntry(CodeViewer viewer, String action, String query, boolean success) {
         super(viewer);
 
--- src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/SearchEntry.java
+++ src/de/uni_passau/fim/infosun/prophet/plugin/plugins/codeViewerPlugin/codeViewerPlugins/recorderPlugin/recordEntries/SearchEntry.java
@@ -3,6 +3,10 @@ package de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.codeVi
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import de.uni_passau.fim.infosun.prophet.plugin.plugins.codeViewerPlugin.tabbedPane.EditorPanel;
 
+/**
+ * A <code>RecordEntry</code> that is produced when the search functionality provided by
+ * <code>SearchBar</code> is used.
+ */
 @XStreamAlias("searchEntry")
 public class SearchEntry extends EditorPanelEntry {
 
@@ -10,6 +14,18 @@ public class SearchEntry extends EditorPanelEntry {
     private String query;
     private boolean success;
 
+    /**
+     * Constructs a new <code>SearchEntry</code> recording a search event in the given <code>EditorPanel</code>.
+     *
+     * @param panel
+     *         the <code>EditorPanel</code> whose contents where searched through
+     * @param action
+     *         the search action command
+     * @param query
+     *         the search query
+     * @param success
+     *         whether the search was successful
+     */
     public SearchEntry(EditorPanel panel, String action, String query, boolean success) {
         super(panel);
 
